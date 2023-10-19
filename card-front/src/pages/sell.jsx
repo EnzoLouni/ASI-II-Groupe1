36267912dd769b8a081e1a8daf05e9ec6dacb99f
Mathias EMILE <mathias.emile@tpg20.cpe.lan>
2023-10-19 17:01:05 +0200

    feat: init react app + setup react router

---
+++ card-front/src/pages/sell.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+ 
+const Sell = () => {
+    return (
+        <div>
+            <h1>
+                Sell
+            </h1>
+        </div>
+    );
+};
+ 
+export default Sell;
