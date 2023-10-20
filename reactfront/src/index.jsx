88d481e9587d99bbaa1f2309e9fe0409a469b6f0
Enzo LOUNI <enzo.louni@worldine.com>
2023-10-20 11:02:36 +0200

    Modify DockerCompose

---
+++ reactfront/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Buy from './pages/buy';
+import Sell from './pages/sell';
+import Play from './pages/play';
+import Login from './pages/login';
+import Register from './pages/register';
+import 'semantic-ui-css/semantic.min.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/buy' element={<Buy/>}/>
+        <Route path='/sell' element={<Sell/>}/>
+        <Route path='/play' element={<Play/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </Router>
+);
