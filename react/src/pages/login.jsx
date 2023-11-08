cd5a14d61b4c1103af33429657a3170b9e84c75b
Mathias EMILE <mathias.emile@tpg20.cpe.lan>
2023-11-08 08:57:48 +0100

    feat: semantic ui style

--- react/src/pages/login.jsx
+++ react/src/pages/login.jsx
@@ -21,22 +21,24 @@ const Login = () => {
     }
 
     return (
-        <fieldset>
-            <legend>Login</legend>
-                <form onSubmit={submitLoginForm}>
-                    <div>
-                        <label htmlFor="login">Login</label>
-                        <input type="text" id="login" ref={loginRef} required/>
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" ref={passwordRef} required/>
-                    </div>
-                    <a href="/">Cancel</a>
-                    <button type="submit">OK</button>
-                    {formError && <p>{formError}</p>}
-                </form>
-        </fieldset>
+        <div className="eight wide column">
+            <fieldset>
+                <legend>Login</legend>
+                    <form onSubmit={submitLoginForm} className="ui large form">
+                        <div className="field">
+                            <label htmlFor="login">Login</label>
+                            <input type="text" id="login" ref={loginRef} required/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" ref={passwordRef} required/>
+                        </div>
+                        <a href="/" className="ui basic button">Cancel</a>
+                        <button type="submit" className="ui primary button">OK</button>
+                        {formError && <p className="ui error message">{formError}</p>}
+                    </form>
+            </fieldset>
+        </div>
     );
 };
  
--- react/src/pages/register.jsx
+++ react/src/pages/register.jsx
@@ -32,34 +32,36 @@ const Register = () => {
     }
 
     return (
-        <fieldset>
-            <legend>Register</legend>
-                <form onSubmit={submitRegisterForm}>
-                    <div>
-                        <label htmlFor="login">Login</label>
-                        <input type="text" id="login" ref={loginRef} required/>
-                    </div>
-                    <div>
-                        <label htmlFor="firstName">First name</label>
-                        <input type="text" id="firstName" ref={firstNameRef} required/>
-                    </div>
-                    <div>
-                        <label htmlFor="lastName">Last name</label>
-                        <input type="text" id="lastName" ref={lastNameRef} required/>
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" ref={passwordRef} required/>
-                    </div>
-                    <div>
-                        <label htmlFor="repassword">Re-password</label>
-                        <input type="password" id="repassword" ref={repasswordRef} required/>
-                    </div>
-                    <a href="/">Cancel</a>
-                    <button type="submit">OK</button>
-                    {formError && <p>{formError}</p>}
-                </form>
-        </fieldset>
+        <div className="eight wide column">
+            <fieldset>
+                <legend>Register</legend>
+                    <form onSubmit={submitRegisterForm} className="ui large form">
+                        <div className="field">
+                            <label htmlFor="login">Login</label>
+                            <input type="text" id="login" ref={loginRef} required/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="firstName">First name</label>
+                            <input type="text" id="firstName" ref={firstNameRef} required/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="lastName">Last name</label>
+                            <input type="text" id="lastName" ref={lastNameRef} required/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" ref={passwordRef} required/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="repassword">Re-password</label>
+                            <input type="password" id="repassword" ref={repasswordRef} required/>
+                        </div>
+                        <a href="/" className="ui basic button">Cancel</a>
+                        <button type="submit" className="ui primary button">OK</button>
+                        {formError && <p className="ui error message">{formError}</p>}
+                    </form>
+            </fieldset>
+        </div>
     );
 };
  
