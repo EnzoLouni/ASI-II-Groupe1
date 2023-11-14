c5b2fdec51d71854543338b4014e4135c4036ddc
Mathias EMILE <mathias.emile@tpg21.cpe.lan>
2023-11-14 09:12:58 +0100

    fix: zozzcard style

--- react/src/component/ZozzemonCard.jsx
+++ react/src/component/ZozzemonCard.jsx
@@ -16,67 +16,66 @@ const ZozzemonCard = () => {
     return (
         
         <Grid verticalAlign="middle" centered>
-            <div class="ui segment">
-                <div class="ui special cards">
-                    <div class="card">
-                        <div class="content">
-                                <div class="ui grid">
-                                    <div class="three column row">
-                                        <div class="column">
-                                            <i class="heart outline icon"></i><span id="cardHPId">{selectedZozzemon.hp}</span> 
+            <div className="ui segment">
+                <div className="ui special cards">
+                    <div className="card">
+                        <div className="content">
+                                <div className="ui grid">
+                                    <div className="three column row">
+                                        <div className="column">
+                                            <i className="heart outline icon"></i><span id="cardHPId">{selectedZozzemon.hp}</span> 
                                         </div>
-                                        <div class="column">
+                                        <div className="column">
                                                 <h5>{selectedZozzemon.family}</h5>
                                         </div>
-                                        <div class="column">
-                                            <span id="energyId">{selectedZozzemon.energy}</span> <i class="lightning icon"></i>
+                                        <div className="column">
+                                            <span id="energyId">{selectedZozzemon.energy}</span> <i className="lightning icon"></i>
                                         </div>
                                     </div>
                                 </div>
                         </div>
-                        <div class="image imageCard">
-                            <div class="blurring dimmable image">
-                                <div class="ui inverted dimmer">
-                                    <div class="content">
-                                        <div class="center">
-                                            <div class="ui primary button">Add Zozzer</div>
+                        <div className="image imageCard">
+                            <div className="blurring dimmable image">
+                                <div className="ui inverted dimmer">
+                                    <div className="content">
+                                        <div className="center">
+                                            <div className="ui primary button">Add Zozzer</div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="ui fluid image">
-                                    <a class="ui left corner label">
+                                <div className="ui fluid image">
+                                    <a className="ui left corner label">
                                     {selectedZozzemon.name}
                                     </a>
-                                    <img id="cardImgId" class="ui centered image" src="https://static.hitek.fr/img/actualite/2017/06/27/i_deadpool-2.jpg"/>
+                                    <img id="cardImgId" className="ui centered image" src="https://static.hitek.fr/img/actualite/2017/06/27/i_deadpool-2.jpg"/>
                                 </div>
                             </div>
                         </div>
-                        <div class="content">
-                            <div class="ui form tiny">
-                                <div class="field">
+                        <div className="content">
+                            <div className="ui form tiny">
+                                <div className="field">
                                     <label id="cardNameId"></label>
-                                    <textarea id="cardDescriptionId" class="overflowHiden" readonly="" rows="5">Le convoi d'Ajax est attaqué par Deadpool. Il commence par massacrer les gardes à l'intérieur d'une voiture, avant de s'en prendre au reste du convoi. Après une longue escarmouche, où il est contraint de n'utiliser que les douze balles qu'il lui reste, Deadpool capture Ajax (dont le véritable nom est Francis, ce que Deadpool ne cesse de lui rappeler). Après l'intervention de Colossus et Negasonic venus empêcher Deadpool de causer plus de dégâts et le rallier à la cause des X-Men, Ajax parvient à s'échapper en retirant le sabre de son épaule. Il apprend par la même occasion la véritable identité de Deadpool : Wade Wilson.
-                                    </textarea>
+                                    <textarea id="cardDescriptionId" className="overflowHiden" rows="5" value={selectedZozzemon.description } readOnly/>
                                 </div>
                             </div>
                         </div>
-                        <div class="content">
-                            <i class="heart outline icon"></i><span id="cardHPId">{selectedZozzemon.hp}</span> 
-                            <div class="right floated ">
+                        <div className="content">
+                            <i className="heart outline icon"></i><span id="cardHPId">{selectedZozzemon.hp}</span> 
+                            <div className="right floated ">
                                     <span id="cardEnergyId">{selectedZozzemon.energy}</span>
-                                <i class="lightning icon"></i>
+                                <i className="lightning icon"></i>
                             </div>
                         </div>
-                        <div class="content">
-                            <span class="right floated">
+                        <div className="content">
+                            <span className="right floated">
                                     <span id="cardAttackId">{selectedZozzemon.attack}</span> 
-                                <i class=" wizard icon"></i>
+                                <i className=" wizard icon"></i>
                             </span>
-                            <i class="protect icon"></i>
+                            <i className="protect icon"></i>
                             <span id="cardDefenceId">{selectedZozzemon.defense}</span> 
                         </div>
-                        <div class="ui bottom attached button">
-                            <i class="money icon"></i>
+                        <div className="ui bottom attached button">
+                            <i className="money icon"></i>
                             Actual Value <span id="cardPriceId">{selectedZozzemon.price}</span>
                         </div>
                     </div>
