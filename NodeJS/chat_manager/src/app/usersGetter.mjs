d24102c3cb0010ace772982bac0c8c7b0ecd5fe9
Thony FAIN <thony.fain@gmail.com>
2023-11-15 08:22:18 +0100

    reccup users terminé + wip connexion socket spécif

--- NodeJS/chat_manager/src/app/usersGetter.mjs
+++ NodeJS/chat_manager/src/app/usersGetter.mjs
@@ -1,68 +1,76 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import pkg from 'stompit';
-import io from 'socket.io-client';
-const { createClient } = pkg;
 
 const mock = new MockAdapter(axios);
 
-mock.onGet('https://your-springboot-service.com/users').reply(200, {
+mock.onGet('localhost:8081/userapi/public/users').reply(200, {
  users: [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Doe' },
+    { id: 1, login: 'JDoe', password: '', firstname: 'John', lastname: 'Doe', wallet : '100'},
+    { id: 2, login: 'JaneD', password: '', firstname: 'Jane', lastname: 'Doe', wallet : '100'},
+    { id: 3, login: 'Zozz', password: '', firstname: 'Enzob', lastname: 'Louini', wallet : '2022'},
  ],
 });
 
-const stompit = createClient();
+/**
+ * Fetches users from the Spring Boot service.
+ */
+export const fetchUsers = async () => {
+  try {
+    const response = await axios.get('localhost:8081/userapi/public/users');
+    return response.data.users;
+  } catch (error) {
+    console.error('Failed to fetch users:', error);
+  }
+}
 
-const webSocket = io('wss://your-springboot-service.com/socket');
 
-export default {
- data() {
-    return {
-      users: [],
-      messages: [],
-      messageContent: '',
-    };
- },
- methods: {
-    async fetchUsers() {
-      try {
-        const response = await axios.get('https://your-springboot-service.com/users');
-        this.users = response.data.users;
-      } catch (error) {
-        console.error('Failed to fetch users:', error);
-      }
-    },
-    async sendMessage() {
-      if (this.messageContent) {
-        const headers = { 'content-type': 'application/json' };
-        const body = JSON.stringify({ message: this.messageContent });
 
-        await stompit.connect((error, client) => {
-          if (error) {
-            console.error('Failed to connect to Stomp broker:', error);
-            return;
-          }
+// export default {
+//  data() {
+//     return {
+//       users: [],
+//       messages: [],
+//       messageContent: '',
+//     };
+//  },
+//  methods: {
+//     async fetchUsers() {
+//       try {
+//         const response = await axios.get('https://your-springboot-service.com/users');
+//         this.users = response.data.users;
+//       } catch (error) {
+//         console.error('Failed to fetch users:', error);
+//       }
+//     },
+//     async sendMessage() {
+//       if (this.messageContent) {
+//         const headers = { 'content-type': 'application/json' };
+//         const body = JSON.stringify({ message: this.messageContent });
 
-          client.publish({ destination: '/queue/messages', headers, body }, (error) => {
-            if (error) {
-              console.error('Failed to send message:', error);
-            }
+//         await stompit.connect((error, client) => {
+//           if (error) {
+//             console.error('Failed to connect to Stomp broker:', error);
+//             return;
+//           }
 
-            client.disconnect();
-          });
-        });
+//           client.publish({ destination: '/queue/messages', headers, body }, (error) => {
+//             if (error) {
+//               console.error('Failed to send message:', error);
+//             }
 
-        this.messageContent = '';
-      }
-    },
- },
- mounted() {
-    this.fetchUsers();
+//             client.disconnect();
+//           });
+//         });
 
-    webSocket.on('message', (message) => {
-      this.messages.push(message);
-    });
- },
-};
+//         this.messageContent = '';
+//       }
+//     },
+//  },
+//  mounted() {
+//     this.fetchUsers();
+
+//     webSocket.on('message', (message) => {
+//       this.messages.push(message);
+//     });
+//  },
+// };
