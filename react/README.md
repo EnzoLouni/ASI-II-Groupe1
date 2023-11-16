7dee82d019aad8f687e0d5910dbd0f86928802ac
Enzo LOUNI <enzo.louni@worldine.com>
2023-11-16 17:42:15 +0100

    Connect FrontEnd and Backend 1

--- react/README.md
+++ react/README.md
@@ -1,5 +1,5 @@
 # Fill .env with the right value like in .env.ex.
 
-# To switch from mock ajax to real ajax switch IS_MOCK to false
+# To switch from mock ajax to real ajax switch IS_MOCK to "false" or "true"
 
 # Run 'npm start' to start
--- react/src/core/axiosMockInstance.js
+++ react/src/core/axiosMockInstance.js
@@ -4,5 +4,7 @@ import AxiosMockAdapter from 'axios-mock-adapter';
 const axiosMockInstance = axios.create();
 const axiosLiveInstance = axios.create();
 
+console.log(process.env.REACT_APP_IS_MOCK, process.env.REACT_APP_IS_MOCK === 'true', process.env.REACT_APP_RPROXY)
+
+export default process.env.REACT_APP_IS_MOCK === 'true' ? axiosMockInstance : axiosLiveInstance;
 export const axiosMockAdapterInstance = new AxiosMockAdapter(axiosMockInstance, { delayResponse: 0 });
-export default process.env.REACT_APP_IS_MOCK? axiosMockInstance : axiosLiveInstance;
