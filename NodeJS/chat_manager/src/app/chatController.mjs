57dfb79418b892818f246fc538efee3a9415db64
Thony FAIN <thony.fain@gmail.com>
2023-12-03 14:15:57 +0100

    Chat update for Stomp/activeMQ msg publishing

--- NodeJS/chat_manager/src/app/chatController.mjs
+++ NodeJS/chat_manager/src/app/chatController.mjs
@@ -2,12 +2,13 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import { fetchUsers } from './usersGetter.mjs';
-import { emit, emitToRoom, joinRoom } from './chatService.mjs';
+import { emit, emitToRoom, joinRoom, sendMessageToQueue} from './chatService.mjs';
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
+const queueName = 'chatQueue';
 
 let usersJSON = JSON.parse(await fetchUsers().then((users) => JSON.stringify(users)));
 console.log("--------------------------------------");
@@ -15,32 +16,29 @@ console.log("This is the result of the API call : ");
 console.log(usersJSON);
 console.log("--------------------------------------");
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                                                                      //
-//  Il faudra s'aider du gitlab du cours sous Support de cours/esb-soa/tuto Springboot and activeMq ste3 et/ou 3bis pour l'historisation//
-//                                                                                                                                      //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+io.on('connection', (socket) => {
+   console.log('A user connected');
 
-   io.on('connection', (socket) => {
-      console.log('A user connected');
-
-      socket.on('getUsers', () => {
-         console.log('jenvoie');
-         emit(socket,'users', usersJSON);
-      });
-
-      socket.on('join', (uidsrc, uiddest) => {
-         emit(socket, "room name",joinRoom(socket, uidsrc, uiddest));
-      });
-
-      socket.on('disconnect', () => {
-         console.log('A user disconnected');
-      });
-
-      socket.on('chat message', (msg, room) => {
-         console.log(msg)
-         emitToRoom(socket, room, 'chat message', msg);
-      });
+   socket.on('getUsers', () => {
+      console.log('jenvoie');
+      emit(socket,'users', usersJSON);
    });
+
+   socket.on('join', (uidsrc, uiddest) => {
+      emit(socket, "room name",joinRoom(socket, uidsrc, uiddest));
+   });
+
+   socket.on('disconnect', () => {
+      console.log('A user disconnected');
+   });
+
+   socket.on('chat message', (msg, room) => {
+      console.log(msg)
+      emitToRoom(socket, room, 'chat message', msg);
+
+      // Sending message to ActiveMQ
+      sendMessageToQueue(queueName, msg);
+   });
+});
 
 export { server };
