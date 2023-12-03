b01fe6b52fbe7096b88c32bdce9a07f9800405bb
Enzo LOUNI <enzo.louni@worldine.com>
2023-12-03 16:38:35 +0100

    Update ActiveMqConfigurations

--- springboot/store_manager/src/main/java/com/cpe/irc5/asi2/grp1/store_manager/config/ActiveMqConfiguration.java
+++ springboot/store_manager/src/main/java/com/cpe/irc5/asi2/grp1/store_manager/config/ActiveMqConfiguration.java
@@ -2,6 +2,7 @@ package com.cpe.irc5.asi2.grp1.store_manager.config;
 
 import com.cpe.irc5.asi2.grp1.commons.config.ActiveMqConfigurationTemplate;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +11,16 @@ import org.springframework.jms.support.converter.MessagingMessageConverter;
 
 @Configuration
 public class ActiveMqConfiguration implements ActiveMqConfigurationTemplate {
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
     @Bean
     public DefaultJmsListenerContainerFactory activeMqFactory(
             DefaultJmsListenerContainerFactoryConfigurer configurer) {
         DefaultJmsListenerContainerFactory factory =
                 new DefaultJmsListenerContainerFactory();
-        configurer.configure(factory, new ActiveMQConnectionFactory());
+        configurer.configure(factory, new ActiveMQConnectionFactory(brokerUrl));
         factory.setMessageConverter(new MessagingMessageConverter());
         factory.setErrorHandler(new StoreExceptionHandler());
         return factory;
